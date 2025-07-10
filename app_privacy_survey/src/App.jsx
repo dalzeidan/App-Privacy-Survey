6945aba777821ba37fa138bd561e52e82ae96c69
Ajknight121 <aknig2@uic.edu>
2025-07-10 14:49:55 -0500

    add index.css and remove theme preference

--- app_privacy_survey/src/App.jsx
+++ app_privacy_survey/src/App.jsx
@@ -1,6 +1,7 @@
 import { SurveyContextProvider } from "./SurveyContext";
 import {BrowserRouter, Routes, Route} from "react-router"
 import InnerApp from "./InnerApp";
+import './index.css'
 
 export default function App() {
   return (
