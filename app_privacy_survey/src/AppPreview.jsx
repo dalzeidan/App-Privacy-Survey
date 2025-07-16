c0c6ace7a8b25585b2926d4b8ad5048921e28927
Ajknight121 <aknig2@uic.edu>
2025-07-16 15:18:10 -0500

    comment out app privacy

--- app_privacy_survey/src/AppPreview.jsx
+++ app_privacy_survey/src/AppPreview.jsx
@@ -34,7 +34,7 @@ export default function AppPreview({ app }) {
         {parsedAppDescription}
       </div>
 
-      <div className="app-privacy">
+      {/* <div className="app-privacy">
         <h3>App Privacy</h3>
         <div>The developer, Apple, indicated that the app’s privacy practices may include handling of data as described below. For more information, see the developer’s privacy policy.</div>
 
@@ -44,7 +44,7 @@ export default function AppPreview({ app }) {
           <div>DATA TYPES</div>
         </div>
 
-      </div>
+      </div> */}
 
       {/* <div className="information">
         <h3>Information</h3>
