6d048eb75032326e7f10e8059d35f7e585905573
danalz5 <danielalzeidan@gmail.com>
2025-08-29 12:04:04 -0500

    vite config possible fix

--- app_privacy_survey/vite.config.js
+++ app_privacy_survey/vite.config.js
@@ -1,23 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    lib: {
-      entry: 'index.js',
-      name: 'App-Privacy-Survey',
-      fileName: (format) => `App-Privacy-Survey.${format}.js`,
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-        },
-      },
-    },
+    outDir: 'dist', // default anyway
   },
-});
+})
